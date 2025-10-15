94154b98fd8a6cff90f26ff433a6150cf91c6541
QAverage <gorbatenko.sam@gmail.com>
2025-10-15 17:14:10 +0300

    add property's to gitignore

--- src/main/java/data/Users.java
+++ src/main/java/data/Users.java
@@ -1,7 +1,9 @@
 package data;
 
+import utils.PropertyReader;
+
 public class Users {
 
-    public static final String EMAIL_ADDRESS = System.getProperty("EMAIL_ADDRESS");
-    public static final String PASSWORD = System.getProperty("PASSWORD");
+    public static final String EMAIL_ADDRESS = System.getProperty("EMAIL_ADDRESS", PropertyReader.getProperty("email"));
+    public static final String PASSWORD = System.getProperty("PASSWORD", PropertyReader.getProperty("password"));
 }
