8047bf844152f0b8cba33fc98443dd0fbb058e27
QAverage <gorbatenko.sam@gmail.com>
2025-10-19 19:54:45 +0300

    test commit

--- src/main/java/data/Users.java
+++
@@ -1,9 +0,0 @@
-package data;
-
-import utils.PropertyReader;
-
-public class Users {
-
-    public static String EMAIL_ADDRESS = System.getProperty("EMAIL_ADDRESS", PropertyReader.getProperty("email"));
-    public static String PASSWORD = System.getProperty("PASSWORD", PropertyReader.getProperty("password"));
-}
--- src/main/java/urlPaths/UrlPaths.java
+++
@@ -1,7 +0,0 @@
-package urlPaths;
-
-public class UrlPaths {
-
-    public static final String BASE_URL = "https://www.w3schools.com/";
-    public static final String BASE_URL_AFTER_AUTH = "https://pathfinder.w3schools.com/";
-}
