40d3e275e842cac59faf3f707530aa4e0c4cb876
QAverage <gorbatenko.sam@gmail.com>
2025-10-20 10:05:13 +0300

    fix problems with allure by update git

--- src/main/java/data/Users.java
+++ src/main/java/data/Users.java
@@ -4,6 +4,6 @@ import utils.PropertyReader;
 
 public class Users {
 
-    public static String EMAIL_ADDRESS = System.getProperty("EMAIL_ADDRESS", PropertyReader.getProperty("email"));
+    public static String EMAIL_ADDRESS = System.getProperty("EMAIL", PropertyReader.getProperty("email"));
     public static String PASSWORD = System.getProperty("PASSWORD", PropertyReader.getProperty("password"));
 }
